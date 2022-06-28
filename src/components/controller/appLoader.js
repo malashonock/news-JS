b5488a5cb2b31d19e7c4b1b3289c124d142ff587
Yan Malashonak <malashonock@gmail.com>
2022-06-28 23:47:01 +0300

    init: put in the News API key

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '8de238c8f3604648b8550c6d1d378b82', // получите свой ключ https://newsapi.org/
         });
     }
 }
