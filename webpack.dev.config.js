bdf14a2c2fc7de8f4988e9a4f2bea8b38c962899
Yan Malashonak <malashonock@gmail.com>
2022-07-01 00:32:56 +0300

    fix: incorrect dist path

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,10 +2,11 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    devtool: 'inline-source-map',
+    // devtool: 'inline-source-map',
+    devtool: 'source-map',
     devServer: {
         static: {
-            directory: path.resolve(__dirname, '../dist'),
+            directory: path.resolve(__dirname, './dist'),
         },
     },
 };
